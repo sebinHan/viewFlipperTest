854b7eb926ae85e4adf82047ec58036ca9da4289
Sebin Han <gkstpqls17@gmail.com>
2016-05-24 18:57:00 +0900

    #0 Create Project

---
+++ app/src/main/java/kr/hs/emirim/sbb9918/viewflippertest/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.sbb9918.viewflippertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
